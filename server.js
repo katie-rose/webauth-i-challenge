c537ec4254e051a9fe0466ed057b41312b0c56ef
Katie Rose <katiefelten@gmail.com>
2019-08-19 17:04:02 -0400

    :zap: added base files

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const server = express();
+const helmet = require("helmet");
+
+const authRouter = require("./routers/authRouter");
+const userRouter = require("./routers/userRouter");
+
+server.use(helmet());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("It's working. IT'S WORKING!");
+});
+
+server.use("/api/auth/", authRouter);
+server.use("/api/users", userRouter);
+
+module.exports = server;
